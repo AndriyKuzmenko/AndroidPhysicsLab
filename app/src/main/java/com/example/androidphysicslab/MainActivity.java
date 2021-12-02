abbf6d362987c4f36909923ad2114d2adcb482c0
Andriy <ak8022@bs.amalnet.k12.il>
2021-12-02 16:07:26 +0200

    Layout first screen

--- app/src/main/java/com/example/androidphysicslab/MainActivity.java
+++ app/src/main/java/com/example/androidphysicslab/MainActivity.java
@@ -3,14 +3,19 @@ package com.example.androidphysicslab;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity
 {
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void logIn(View view)
+    {
+
     }
 }
