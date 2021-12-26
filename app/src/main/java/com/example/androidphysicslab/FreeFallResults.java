87d0eee1f2345a0b6d847a3dc4fb700c0bbdba63
Andriy <ak8022@bs.amalnet.k12.il>
2021-12-26 15:21:16 +0200

    Results activity layout

---
+++ app/src/main/java/com/example/androidphysicslab/FreeFallResults.java
@@ -0,0 +1,16 @@
+package com.example.androidphysicslab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FreeFallResults extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_free_fall_results);
+    }
+}
