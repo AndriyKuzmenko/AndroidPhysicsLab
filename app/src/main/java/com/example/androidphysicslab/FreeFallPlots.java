2c96435a0b05dc557609d13d5eddba5f58bd4c4a
Andriy <ak8022@bs.amalnet.k12.il>
2021-12-27 12:46:51 +0200

    new activity

---
+++ app/src/main/java/com/example/androidphysicslab/FreeFallPlots.java
@@ -0,0 +1,14 @@
+package com.example.androidphysicslab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FreeFallPlots extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_free_fall_plots);
+    }
+}
