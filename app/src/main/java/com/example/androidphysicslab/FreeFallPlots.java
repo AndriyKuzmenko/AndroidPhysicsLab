2411de77fa5088e1d1e04b6ac93185b4d96b6d81
Andriy <ak8022@bs.amalnet.k12.il>
2022-01-06 08:49:31 +0200

    plots

--- app/src/main/java/com/example/androidphysicslab/FreeFallPlots.java
+++ app/src/main/java/com/example/androidphysicslab/FreeFallPlots.java
@@ -2,6 +2,7 @@ package com.example.androidphysicslab;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.jjoe64.graphview.GraphView;
@@ -10,6 +11,8 @@ import com.jjoe64.graphview.series.LineGraphSeries;
 
 public class FreeFallPlots extends AppCompatActivity
 {
+    double[] hList,vList;
+    DataPoint[] vPlot,hPlot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -19,53 +22,29 @@ public class FreeFallPlots extends AppCompatActivity
 
         GraphView graph=(GraphView)findViewById(R.id.heightPlot);
 
+        Intent gi=getIntent();
+        hList=gi.getDoubleArrayExtra("hList");
+        vList=gi.getDoubleArrayExtra("vList");
+        vPlot=new DataPoint[vList.length];
+        hPlot=new DataPoint[vList.length];
+
+        for(int i=0;i<vList.length;i++)
+        {
+            vPlot[i]=new DataPoint((double)i/100,vList[i]);
+        }
+
+        for(int i=0;i<vList.length;i++)
+        {
+            hPlot[i]=new DataPoint((double)i/100,hList[i]);
+        }
+
         try
         {
-            LineGraphSeries<DataPoint> series = new LineGraphSeries< >(new DataPoint[]
-                    {
-                            new DataPoint(-5, 25-2),
-                            new DataPoint(-4.75, 22.5625-2),
-                            new DataPoint(-4.5, 20.25-2),
-                            new DataPoint(-4.25, 18.0625-2),
-                            new DataPoint(-4, 16-2),
-                            new DataPoint(-3.75, 14.0625-2),
-                            new DataPoint(-3.5, 12.25-2),
-                            new DataPoint(-3.25, 10.5625-2),
-                            new DataPoint(-3, 9-2),
-                            new DataPoint(-2.75, 7.5625-2),
-                            new DataPoint(-2.5, 6.25-2),
-                            new DataPoint(-2.25, 5.0625-2),
-                            new DataPoint(-2, 4-2),
-                            new DataPoint(-1.75, 3.0625-2),
-                            new DataPoint(-1.5, 2.25-2),
-                            new DataPoint(-1.25, 1.5625-2),
-                            new DataPoint(-1, 1-2),
-                            new DataPoint(-0.75, 0.5625-2),
-                            new DataPoint(-0.5, 0.25-2),
-                            new DataPoint(-0.25, 0.0625-2),
-                            new DataPoint(0, 0-2),
-                            new DataPoint(0.25, 0.0625-2),
-                            new DataPoint(0.5, 0.25-2),
-                            new DataPoint(0.75, 0.5625-2),
-                            new DataPoint(1, 1-2),
-                            new DataPoint(1.25, 1.5625-2),
-                            new DataPoint(1.5, 2.25-2),
-                            new DataPoint(1.75, 3.0625-2),
-                            new DataPoint(2, 4-2),
-                            new DataPoint(2.25, 5.0625-2),
-                            new DataPoint(2.5, 6.25-2),
-                            new DataPoint(2.75, 7.5625-2),
-                            new DataPoint(3, 9-2),
-                            new DataPoint(3.25, 10.5625-2),
-                            new DataPoint(3.5, 12.25-2),
-                            new DataPoint(3.75, 14.0625-2),
-                            new DataPoint(4, 16-2),
-                            new DataPoint(4.25, 18.0625-2),
-                            new DataPoint(4.5, 20.25-2),
-                            new DataPoint(4.75, 22.5625-2),
-                            new DataPoint(5, 25-2),
-                    });
-            graph.addSeries(series);
+            LineGraphSeries<DataPoint> vSeries = new LineGraphSeries< >(vPlot);
+            graph.addSeries(vSeries);
+
+            LineGraphSeries<DataPoint> hSeries = new LineGraphSeries< >(hPlot);
+            graph.addSeries(hSeries);
         }
         catch (IllegalArgumentException e)
         {
