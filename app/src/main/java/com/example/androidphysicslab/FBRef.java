c50f2e6191274feb15747346bb4f731c22b266ec
Andriy <ak8022@bs.amalnet.k12.il>
2022-01-13 18:07:02 +0200

    authentication part 2

---
+++ app/src/main/java/com/example/androidphysicslab/FBRef.java
@@ -0,0 +1,8 @@
+package com.example.androidphysicslab;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class FBRef
+{
+    public static FirebaseAuth mAuth= FirebaseAuth.getInstance();
+}
--- app/src/main/java/com/example/androidphysicslab/MainActivity.java
+++ app/src/main/java/com/example/androidphysicslab/MainActivity.java
@@ -1,17 +1,36 @@
 package com.example.androidphysicslab;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.ActionMode;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.SearchEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.accessibility.AccessibilityEvent;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity
 {
     Button logIn;
+    EditText phone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -19,15 +38,25 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        phone=(EditText)findViewById(R.id.phone);
         logIn=(Button)findViewById(R.id.logIn);
+
         Languages.toEnglish();
         changeLanguage();
     }
 
     public void logIn(View view)
     {
-        Intent si=new Intent(this, MenuActivity.class);
-        startActivity(si);
+        String phoneNumber=phone.getText().toString();
+
+        PhoneAuthOptions options =
+                PhoneAuthOptions.newBuilder(FBRef.mAuth)
+                        .setPhoneNumber(phoneNumber)       // Phone number to verify
+                        .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+                        .setActivity(this)                 // Activity (for callback binding)
+                        //.setCallbacks(mCallback)          // OnVerificationStateChangedCallbacks
+                        .build();
+        PhoneAuthProvider.verifyPhoneNumber(options);
     }
 
     @Override
@@ -67,5 +96,11 @@ public class MainActivity extends AppCompatActivity
         super.onPause();
 
         finish();
+    }
+
+    public void nextActivity()
+    {
+        Intent si=new Intent(this, MenuActivity.class);
+        startActivity(si);
     }
 }
