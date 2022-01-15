2e1d5639870dae070b6ed5e461b36f17d38e8709
Andriy <ak8022@bs.amalnet.k12.il>
2022-01-15 17:15:02 +0200

    experiment object part ii

--- app/src/main/java/com/example/androidphysicslab/FreeFallObject.java
+++ app/src/main/java/com/example/androidphysicslab/FreeFallObject.java
@@ -1,6 +1,10 @@
 package com.example.androidphysicslab;
 
+import java.util.ArrayList;
+
 public class FreeFallObject extends Experiment
 {
-
+    public ArrayList<Double> tList;
+    public ArrayList<Double> hList;
+    public ArrayList<Double> vList;
 }
