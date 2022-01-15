300e510ce6c0136800ff0851d60bdfbeabf3c7de
Andriy <ak8022@bs.amalnet.k12.il>
2022-01-15 17:18:26 +0200

    experiment object part iii

--- app/src/main/java/com/example/androidphysicslab/FreeFallObject.java
+++ app/src/main/java/com/example/androidphysicslab/FreeFallObject.java
@@ -4,7 +4,32 @@ import java.util.ArrayList;
 
 public class FreeFallObject extends Experiment
 {
-    public ArrayList<Double> tList;
-    public ArrayList<Double> hList;
-    public ArrayList<Double> vList;
+    private ArrayList<Double> hList;
+    private ArrayList<Double> vList;
+
+    public FreeFallObject(ArrayList<Double> hList, ArrayList<Double> vList)
+    {
+        this.hList=hList;
+        this.vList=vList;
+    }
+
+    public void setHList(ArrayList<Double> hList)
+    {
+        this.hList=hList;
+    }
+
+    public void setVList(ArrayList<Double> vList)
+    {
+        this.vList=vList;
+    }
+
+    public ArrayList<Double> getHList()
+    {
+        return hList;
+    }
+
+    public ArrayList<Double> getVList()
+    {
+        return vList;
+    }
 }
